f3cdbc706cab1f9846694c3c3fa903a2aed82562
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2024-02-27 21:16:14 +0100

    Add files via upload

--- ADmanage.py
+++ ADmanage.py
@@ -1,8 +1,11 @@
 import argparse
 import json
 import ldap3
-from ldap3 import Server, Connection, ALL, LEVEL, MODIFY_ADD, MODIFY_DELETE, MODIFY_REPLACE
-
+from ldap3 import Server, Connection, ALL, SUBTREE, LEVEL, MODIFY_ADD, MODIFY_DELETE, MODIFY_REPLACE
+from impacket.structure import Structure
+import socket
+import dns.resolver
+import datetime
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Process some arguments")
@@ -17,6 +20,136 @@ def parse_arguments():
     args = parser.parse_args()
 
     return args
+
+class DNS_RECORD(Structure):
+    """
+    dnsRecord - used in LDAP
+    [MS-DNSP] section 2.3.2.2
+    """
+    structure = (
+        ('DataLength', '<H-Data'),
+        ('Type', '<H'),
+        ('Version', 'B=5'),
+        ('Rank', 'B'),
+        ('Flags', '<H=0'),
+        ('Serial', '<L'),
+        ('TtlSeconds', '>L'),
+        ('Reserved', '<L=0'),
+        ('TimeStamp', '<L=0'),
+        ('Data', ':')
+    )
+
+class DNS_RPC_RECORD_A(Structure):
+    """
+    DNS_RPC_RECORD_A
+    [MS-DNSP] section 2.2.2.2.4.1
+    """
+    structure = (
+        ('address', ':'),
+    )
+
+    def formatCanonical(self):
+        return socket.inet_ntoa(self['address'])
+
+    def fromCanonical(self, canonical):
+        self['address'] = socket.inet_aton(canonical)
+
+class DNS_RPC_RECORD_TS(Structure):
+    """
+    DNS_RPC_RECORD_TS
+    [MS-DNSP] section 2.2.2.2.4.23
+    """
+    structure = (
+        ('entombedTime', '<Q'),
+    )
+    def toDatetime(self):
+        microseconds = self['entombedTime'] / 10.
+        return datetime.datetime(1601,1,1) + datetime.timedelta(microseconds=microseconds)
+
+def new_record(rtype):
+    nr = DNS_RECORD()
+    nr['Type'] = rtype
+    nr['Serial'] = get_next_serial(dc_ip, domain)
+    nr['TtlSeconds'] = 180
+    # From authoritive zone
+    nr['Rank'] = 240
+    return nr
+
+def get_next_serial(dc, zone):
+    # Create a resolver object
+    dnsresolver = dns.resolver.Resolver()
+    dnsresolver.nameservers = [dc]
+
+    res = dnsresolver.resolve(zone, 'SOA',tcp=False)
+    for answer in res:
+        return answer.serial + 1
+
+
+def get_DNSentries():
+    filter = "(objectClass=*)"
+    conn.search(search_base=dnsroot, search_filter=filter, search_scope=SUBTREE)
+    entries = conn.entries
+    return [entry.entry_dn for entry in entries if "._tcp" not in entry.entry_dn and "._udp" not in entry.entry_dn]
+
+
+def get_raw_entry(target):
+    filter = f'(&(objectClass=dnsNode)(name={target}))'
+    conn.search(search_base=dnsroot, search_filter=filter, attributes=['dnsRecord','dNSTombstoned','name'])
+    for entry in conn.response:
+        if entry['type'] != 'searchResEntry':
+            continue
+        return entry
+
+
+def get_DNSentry(target):
+    record_data = get_raw_entry(target)['raw_attributes']['dnsRecord'][0][-4:]
+    parsed_record = DNS_RPC_RECORD_A(record_data)
+    ip_address = parsed_record.formatCanonical()
+    return {'name': get_raw_entry(target)['attributes']['name'], 'ip': ip_address}
+
+
+def add_DNSentry(target, data):
+    record_dn = f'DC={target},{dnsroot}'
+    node_data = {
+        # Schema is in the root domain (take if from schemaNamingContext to be sure)
+        'objectCategory': f'CN=Dns-Node,CN=Schema,CN=Configuration,{domainroot}',
+        'dNSTombstoned': False,
+        'name': target
+    }
+    record = new_record(1)
+    record['Data'] = DNS_RPC_RECORD_A()
+    record['Data'].fromCanonical(data)
+    node_data['dnsRecord'] = [record.getData()]
+    conn.add(record_dn, ['top', 'dnsNode'], node_data)
+    return get_DNSentry(target)
+
+
+def modify_DNSentry(target, data):
+    targetentry = get_raw_entry(target)
+    records = []
+    for record in targetentry['raw_attributes']['dnsRecord']:
+        dr = DNS_RECORD(record)
+        if dr['Type'] == 1:
+            targetrecord = dr
+        else:
+            records.append(record)
+    targetrecord['Serial'] = get_next_serial(dc_ip, domain)
+    targetrecord['Data'] = DNS_RPC_RECORD_A()
+    targetrecord['Data'].fromCanonical(data)
+    records.append(targetrecord.getData())
+    conn.modify(targetentry['dn'], {'dnsRecord': [(MODIFY_REPLACE, records)]})
+    return get_DNSentry(target)
+
+
+def del_DNSentry(target):
+    targetentry = get_raw_entry(target)
+    diff = datetime.datetime.today() - datetime.datetime(1601,1,1)
+    tstime = int(diff.total_seconds()*10000)
+    # Add a null record
+    record = new_record(0)
+    record['Data'] = DNS_RPC_RECORD_TS()
+    record['Data']['entombedTime'] = tstime
+    conn.modify(targetentry['dn'], {'dnsRecord': [(MODIFY_REPLACE, [record.getData()])],'dNSTombstoned': [(MODIFY_REPLACE, True)]})
 
 
 # List all user, group and computer objects
@@ -196,6 +329,11 @@ def test_login(username, password):
 
 if __name__ == '__main__':
     module_functions = {
+        'get_DNSentries': get_DNSentries,
+        'get_DNSentry': get_DNSentry,
+        'add_DNSentry': add_DNSentry,
+        'modify_DNSentry': modify_DNSentry,
+        'del_DNSentry': del_DNSentry,
         'get_ADobjects': get_ADobjects,
         'get_ADobject': get_ADobject,
         'add_ADobject': add_ADobject,
@@ -219,13 +357,16 @@ if __name__ == '__main__':
     dc_ip = args.dc_ip
     base_dn = args.base_dn
 
+    domainroot = f"DC={domain.split('.')[0]},DC={domain.split('.')[1]}"
+    dnsroot = f"DC={domain},CN=MicrosoftDNS,DC=DomainDnsZones,{domainroot}"
+
     if args.secure:
         dc_url = f"ldaps://{dc_ip}:636"
     else:
         dc_url = f"ldap://{dc_ip}:389"
 
     if not base_dn:
-        base_dn = f"DC={domain.split('.')[0]},DC={domain.split('.')[1]}"
+        base_dn = domainroot
 
     server = Server(dc_url, get_info=ALL)
     conn = Connection(server, user=sam, password=password, auto_bind=True)
