1ed05f21ba6bfb5fde3ca36adec0d92699434eed
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2024-05-12 13:36:42 +0200

    Add files via upload

--- ADmanage.py
+++ ADmanage.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import ldap3
-from ldap3 import Server, Connection, ALL, SUBTREE, LEVEL, MODIFY_ADD, MODIFY_DELETE, MODIFY_REPLACE
+from ldap3 import Server, Connection, ALL, SUBTREE, LEVEL, MODIFY_ADD, MODIFY_DELETE, MODIFY_REPLACE, NTLM
 from impacket.structure import Structure
 import socket
 import dns.resolver
@@ -11,7 +11,8 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="Process some arguments")
     parser.add_argument('-d', '--domain', required=True, help='Domain name of the target system.')
     parser.add_argument('-u', '--username', required=True, help='Username for authentication.')
-    parser.add_argument('-p', '--password', required=True, help='Password for authentication.')
+    parser.add_argument('-p', '--password', help='Password for authentication.')
+    parser.add_argument('-H', '--hashes', help='Hash for authentication.')
     parser.add_argument('--dc-ip', required=True, help='IP address of the Domain Controller.')
     parser.add_argument('-b', '--base-dn', help='Base Distinguished Name (DN) for LDAP queries.')
     parser.add_argument('-s', '--secure', action='store_true', help='Use SSL for secure communication.')
@@ -102,17 +103,20 @@ def get_raw_entry(target):
 
 
 def get_DNSentry(target):
-    record_data = get_raw_entry(target)['raw_attributes']['dnsRecord'][0][-4:]
-    parsed_record = DNS_RPC_RECORD_A(record_data)
-    ip_address = parsed_record.formatCanonical()
-    return {'name': get_raw_entry(target)['attributes']['name'], 'ip': ip_address}
+    if not get_raw_entry(target):
+        return "Entry doesn't exist"
+
+        record_data = get_raw_entry(target)['raw_attributes']['dnsRecord'][0][-4:]
+        parsed_record = DNS_RPC_RECORD_A(record_data)
+        ip_address = parsed_record.formatCanonical()      
+        return {'name': get_raw_entry(target)['attributes']['name'], 'ip': ip_address}
 
 
 def add_DNSentry(target, data):
     record_dn = f'DC={target},{dnsroot}'
     node_data = {
         # Schema is in the root domain (take if from schemaNamingContext to be sure)
-        'objectCategory': f'CN=Dns-Node,CN=Schema,CN=Configuration,{domainroot}',
+        'objectCategory': f'CN=Dns-Node,CN=Schema,CN=Configuration,{domain_root}',
         'dNSTombstoned': False,
         'name': target
     }
@@ -126,6 +130,9 @@ def add_DNSentry(target, data):
 
 def modify_DNSentry(target, data):
     targetentry = get_raw_entry(target)
+    if not targetentry:
+        return "Entry doesn't exist"
+
     records = []
     for record in targetentry['raw_attributes']['dnsRecord']:
         dr = DNS_RECORD(record)
@@ -143,6 +150,9 @@ def modify_DNSentry(target, data):
 
 def del_DNSentry(target):
     targetentry = get_raw_entry(target)
+    if not targetentry:
+        return "Entry doesn't exist"
+
     diff = datetime.datetime.today() - datetime.datetime(1601,1,1)
     tstime = int(diff.total_seconds()*10000)
     # Add a null record
@@ -158,7 +168,10 @@ def get_ADobjects():
     conn.search(base_dn, search_filter, attributes=['*'])
 
     if conn.entries:
-        return conn.entries
+        entries = [json.loads(entry.entry_to_json()) for entry in conn.entries]
+        for entry in entries:
+            entry['attributes'] = {key: str(value[0]) if isinstance(value, list) and len(value) == 1 else value for key, value in entry["attributes"].items()}
+        return entries
 
 
 # Search for user, group, or computer objects with sAMAccountName value
@@ -167,7 +180,10 @@ def get_ADobject(_object):
     conn.search(base_dn, search_filter, attributes=['*'])
 
     if conn.entries:
-        return conn.entries[0]
+        entries = [json.loads(entry.entry_to_json()) for entry in conn.entries]
+        for entry in entries:
+            entry['attributes'] = {key: str(value[0]) if isinstance(value, list) and len(value) == 1 else value for key, value in entry["attributes"].items()}
+        return entries[0]['attributes']
 
 
 # Adding users, computers or groups
@@ -223,19 +239,22 @@ def add_ADobject(ou, attributes):
 
 # Removing users, computers or groups
 def del_ADobject(_object):
-    _object_dn = get_ADobject(_object).distinguishedName
-    if conn.delete(_object_dn[0]):
+    if not get_ADobject(_object):
+        return "Object doesn't exist"
+
+    _object_dn = get_ADobject(_object)['distinguishedName']
+    if conn.delete(_object_dn):
         return 200
-    else:
-        return None
+
 
 # List members of group
-def get_member(group_name):
+def get_members(group_name):
     search_filter = f"(&(objectClass=group)(sAMAccountName={group_name}))"
     conn.search(base_dn, search_filter, attributes=['member'])
 
     if conn.entries:
-        return conn.entries[0].member
+        return conn.entries[0].member.value
+
 
 # List groups of users
 def get_memberOf(username):
@@ -243,44 +262,59 @@ def get_memberOf(username):
     conn.search(base_dn, search_filter, attributes=['memberOf'])
 
     if conn.entries:
-        return conn.entries[0].memberOf
+        return conn.entries[0].memberOf.value
 
 
 # Adding users, computers, or groups to groups
 def add_ADobject_to_group(_object, group):
-    _object_dn = get_ADobject(_object).distinguishedName
-    group_dn = get_ADobject(group).distinguishedName
+    if not get_ADobject(_object) or not get_ADobject(group):
+        return "Group or User doesn't exist"
 
-    conn.modify(group_dn[0], {'member': [(MODIFY_ADD, [_object_dn[0]])]})
-    return get_ADobject(group).member
+    _object_dn = get_ADobject(_object)['distinguishedName']
+    group_dn = get_ADobject(group)['distinguishedName']
+
+    conn.modify(group_dn, {'member': [(MODIFY_ADD, [_object_dn])]})
+    return get_ADobject(group)['member']
 
 
 # Removing users, computers, or groups from groups
 def del_ADobject_from_group(_object, group):
-    _object_dn = get_ADobject(_object).distinguishedName
-    group_dn = get_ADobject(group).distinguishedName
+    if not get_ADobject(_object) or not get_ADobject(group):
+        return "Group or User doesn't exist"
 
-    conn.modify(group_dn[0], {'member': [(MODIFY_DELETE, _object_dn[0])]})
-    return get_ADobject(group).member
+    _object_dn = get_ADobject(_object)['distinguishedName']
+    group_dn = get_ADobject(group)['distinguishedName']
+
+    conn.modify(group_dn, {'member': [(MODIFY_DELETE, _object_dn)]})
+    try:
+        return get_ADobject(group)['member']
+    except KeyError:
+        return None
 
 
 # Updating user, computer, or group attributes.
 def modify_ADobject_attributes(_object, attributes):
     attributes = json.loads(str(attributes).replace("'", "\""))
 
-    _object_dn = get_ADobject(_object).distinguishedName
+    if not get_ADobject(_object):
+        return "Object doesn't exist"
+
+    _object_dn = get_ADobject(_object)['distinguishedName']
 
     for key, value in attributes.items():
-        conn.modify(_object_dn[0], {key: [(MODIFY_REPLACE, [value])]})
+        conn.modify(_object_dn, {key: [(MODIFY_REPLACE, [value])]})
     return get_ADobject(_object)
 
 
 # Reset password (Only work with ssl bind)
 def reset_password(username, password):
-    user_dn = get_ADobject(username).distinguishedName
+    if get_ADobject(username):
+        return "User doesn't exist"
+    
+    user_dn = get_ADobject(username)['distinguishedName']
 
     if server.ssl:
-        if ldap3.extend.microsoft.modifyPassword.ad_modify_password(conn, user_dn[0], password, old_password=None):
+        if ldap3.extend.microsoft.modifyPassword.ad_modify_password(conn, user_dn, password, old_password=None):
             return 200
         else:
             return None
@@ -290,8 +324,11 @@ def reset_password(username, password):
 
 # Enable users or computers
 def enable_ADobject(_object):
+    if not get_ADobject(_object):
+        return "Object doesn't exist"
+
     uacFlag = 2
-    old_uac = get_ADobject(_object).userAccountControl
+    old_uac = get_ADobject(_object)['userAccountControl']
     new_uac = int(str(old_uac)) & ~uacFlag
 
     attributes = {
@@ -304,8 +341,11 @@ def enable_ADobject(_object):
 
 # Disable users or computers
 def disable_ADobject(_object):
+    if not get_ADobject(_object):
+        return "Object doesn't exist"
+
     uacFlag = 2
-    old_uac = get_ADobject(_object).userAccountControl
+    old_uac = get_ADobject(_object)['userAccountControl']
     new_uac = int(str(old_uac)) | uacFlag
 
     attributes = {
@@ -328,6 +368,8 @@ def test_login(username, password):
 
 
 if __name__ == '__main__':
+    from getpass import getpass
+
     module_functions = {
         'get_DNSentries': get_DNSentries,
         'get_DNSentry': get_DNSentry,
@@ -338,7 +380,7 @@ if __name__ == '__main__':
         'get_ADobject': get_ADobject,
         'add_ADobject': add_ADobject,
         'del_ADobject': del_ADobject,
-        'get_member': get_member,
+        'get_members': get_members,
         'get_memberOf': get_memberOf,
         'add_ADobject_to_group': add_ADobject_to_group,
         'del_ADobject_from_group': del_ADobject_from_group,
@@ -352,12 +394,13 @@ if __name__ == '__main__':
 
     domain = args.domain
     username = args.username
-    sam = f"{username}@{domain}"
-    password = args.password
+    sam = f"{domain}\\{username}"
+    password = args.password or args.hashes or getpass("Password:")
     dc_ip = args.dc_ip
-    base_dn = args.base_dn or f"DC={domain.split('.')[0]},DC={domain.split('.')[1]}"
+    domain_root = f"DC={domain.split('.')[0]},DC={domain.split('.')[1]}"
+    base_dn = args.base_dn or domain_root
 
-    dnsroot = f"DC={domain},CN=MicrosoftDNS,DC=DomainDnsZones,{domainroot}"
+    dnsroot = f"DC={domain},CN=MicrosoftDNS,DC=DomainDnsZones,{domain_root}"
 
     if args.secure:
         dc_url = f"ldaps://{dc_ip}:636"
@@ -365,12 +408,12 @@ if __name__ == '__main__':
         dc_url = f"ldap://{dc_ip}:389"
 
     server = Server(dc_url, get_info=ALL)
-    conn = Connection(server, user=sam, password=password, auto_bind=True)
+    conn = Connection(server, user=sam, password=password, authentication=NTLM, auto_bind=True)
 
     module = args.module
     selected_function = module_functions.get(module, None)
 
     if args.data:
-        print(selected_function(*args.data))
+        print(json.dumps(selected_function(*args.data), indent=4))
     else:
-        print(selected_function())
+        print(json.dumps(selected_function(), indent=4))
